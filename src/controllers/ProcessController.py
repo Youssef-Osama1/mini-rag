6df5dfb8c91d1f1c6055404b86f4c96f88b6ecc5
Youssef Osama <youssefosama20055@gmail.com>
2026-04-02 23:46:58 +0200

    Enhance Data Pipeline

--- src/controllers/ProcessController.py
+++ src/controllers/ProcessController.py
@@ -20,6 +20,9 @@ class ProcessController(BaseController):
         file_ext = self.get_file_extension(file_id=file_id)
         file_path = os.path.join(self.project_path, file_id)
 
+        if not os.path.exists(file_path):
+            return None
+
         if file_ext == ProcessingEnum.TXT.value:
             return TextLoader(file_path, encoding='utf-8')
 
@@ -31,7 +34,10 @@ class ProcessController(BaseController):
     def get_file_content(self, file_id: str):
 
         loader = self.get_file_loader(file_id=file_id)
-        return loader.load()
+        if loader:
+            return loader.load()
+        
+        return None
     
     def process_file_content(self, file_content: list, file_id: str, chunk_size: int = 100, overlap_size: int = 20):
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=overlap_size, length_function=len)
