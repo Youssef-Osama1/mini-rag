57475e31a544ca7afbb9171a8c081b36b410c586
Youssef Osama <youssefosama20055@gmail.com>
2026-04-02 21:41:49 +0200

    set indices for projects + chunks

--- src/routes/data.py
+++ src/routes/data.py
@@ -25,7 +25,7 @@ data_router = APIRouter(
 @data_router.post("/upload/{project_id}")
 async def upload_data(request: Request, project_id: str, file: UploadFile, app_settings: Settings = Depends(get_settings)):
 
-    project_model = ProjectModel(db_client = request.app.db_client)
+    project_model = await ProjectModel.create_instance(db_client = request.app.db_client)
     project = await project_model.get_project_or_create_one(project_id=project_id)
 
     # validate uploaded file
@@ -77,7 +77,7 @@ async def process_endpoint(request: Request, project_id: str, process_request: P
     overlap_size = process_request.overlap_size
     do_reset = process_request.do_reset
 
-    project_model = ProjectModel(db_client = request.app.db_client)
+    project_model = await ProjectModel.create_instance(db_client = request.app.db_client)
     project = await project_model.get_project_or_create_one(project_id=project_id)
 
     process_controller = ProcessController(project_id=project_id)
@@ -108,7 +108,7 @@ async def process_endpoint(request: Request, project_id: str, process_request: P
         for i, chunk in enumerate(file_chunks)
     ]
 
-    chunk_model = ChunkModel(db_client = request.app.db_client)
+    chunk_model = await ChunkModel.create_instance(db_client = request.app.db_client)
 
     if do_reset == 1:
         _ = await chunk_model.delete_chunks_by_project_id(project_id=project.id)
